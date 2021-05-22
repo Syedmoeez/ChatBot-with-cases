7e34a6798077b90b92ae45671b5e946d5b6e3e53
Syedmoeez <84286252+Syedmoeez@users.noreply.github.com>
2021-05-21 20:42:06 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let mess = prompt("Enter message of the following: \n Phone Number \n Address \n Email \n Website ");
+
+switch(mess){
+    case "Phone Number" :
+        alert("why you want to know");
+        break;
+
+    case "Address" :
+        alert("somewhere..somewhere");
+        break;
+
+    case "Email" :
+        alert("noreply......@gmail.com");
+        break;
+
+    case "Website" :
+        alert("https://something....something.com");
+        break;
+    default:
+        alert("data not found");
+}
